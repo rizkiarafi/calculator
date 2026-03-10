c45f73bd0dba383ec3efb90800b742c6ffec72f8
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-11 05:32:29 +0700

    Add all operation functions that operates 2 numbers

---
+++ app.js
@@ -0,0 +1,17 @@
+const operatedNumbers = [10, 10];
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function division(num1, num2) {
+  return num1 / num2;
+}

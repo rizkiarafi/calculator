f785ca060ce0725b6951de64d6c1f2cea2d576a8
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-11 22:35:06 +0700

    Add logic to display the numbers through clicking the numbers

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const buttonContainer = document.querySelector("#button-container");
+const numberInput = document.querySelector("#number-input");
+
+buttonContainer.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.classList[0] === "number-btn") {
+    numberInput.value += target.textContent;
+  }
+});
+
 const operatedNumbers = [10, 10];
 
 function add(num1, num2) {

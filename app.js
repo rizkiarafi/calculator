9c5b8585d3c946bea4fdc614531250103a9addc7
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-12 05:47:51 +0700

    Remove the previous operation logic

--- app.js
+++ app.js
@@ -7,26 +7,9 @@ buttonContainer.addEventListener("click", (e) => {
   if (target.id === "number-btn") {
     numberInput.value += target.textContent;
   } else if (target.id === "add-btn") {
-    operate(operatedNumbers, (num1, num2) => add(num1, num2));
   } else if (target.id === "multiply-btn") {
-    operate(operatedNumbers, (num1, num2) => multiply(num1, num2));
   }
 });
-
-function operate(numbers, getOperation) {
-  numbers.push(Number(numberInput.value));
-
-  if (operatedNumbers.length > 1) {
-    showResult();
-  } else {
-    numberInput.value = "";
-  }
-
-  function showResult() {
-    numberInput.value = getOperation(numbers[0], numbers[1]);
-    operatedNumbers.length = 0;
-  }
-}
 
 function add(num1, num2) {
   return num1 + num2;

61475c4d8d5f37163400bffa4c856ed3114fc898
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-12 05:42:21 +0700

    Add the add and multiply operation

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const buttonContainer = document.querySelector("#button-container");
 const numberInput = document.querySelector("#number-input");
+const operatedNumbers = [];
 
 buttonContainer.addEventListener("click", (e) => {
   const target = e.target;
-  if (target.classList[0] === "number-btn") {
+  if (target.id === "number-btn") {
     numberInput.value += target.textContent;
+  } else if (target.id === "add-btn") {
+    operate(operatedNumbers, (num1, num2) => add(num1, num2));
+  } else if (target.id === "multiply-btn") {
+    operate(operatedNumbers, (num1, num2) => multiply(num1, num2));
   }
 });
 
-const operatedNumbers = [10, 10];
+function operate(numbers, getOperation) {
+  numbers.push(Number(numberInput.value));
+
+  if (operatedNumbers.length > 1) {
+    showResult();
+  } else {
+    numberInput.value = "";
+  }
+
+  function showResult() {
+    numberInput.value = getOperation(numbers[0], numbers[1]);
+    operatedNumbers.length = 0;
+  }
+}
 
 function add(num1, num2) {
   return num1 + num2;

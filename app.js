ca944ac40982f10c45adc7b98c766dba7f1f6075
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-12 07:01:44 +0700

    Add new add and multiply logic which shows the result

--- app.js
+++ app.js
@@ -2,14 +2,35 @@ const buttonContainer = document.querySelector("#button-container");
 const numberInput = document.querySelector("#number-input");
 const operatedNumbers = [];
 
+let hasPreviousNumber = false;
+let currentOperation = "";
+
 buttonContainer.addEventListener("click", (e) => {
   const target = e.target;
-  if (target.id === "number-btn") {
+  if (target.classList[0] === "number-btn") {
+    if (hasPreviousNumber) numberInput.value = "";
+    hasPreviousNumber = false;
     numberInput.value += target.textContent;
-  } else if (target.id === "add-btn") {
-  } else if (target.id === "multiply-btn") {
+  } else if (target.classList[0] === "operation-btn") {
+    operatedNumbers.push(Number(numberInput.value));
+
+    if (operatedNumbers.length > 1) {
+      showResult(currentOperation);
+    }
+    currentOperation = target.id;
+    hasPreviousNumber = true;
   }
 });
+
+function showResult(id) {
+  if (id === "add-btn") {
+    numberInput.value = add(operatedNumbers[0], operatedNumbers[1]);
+  } else if (id === "multiply-btn") {
+    numberInput.value = multiply(operatedNumbers[0], operatedNumbers[1]);
+  }
+  operatedNumbers.length = 0;
+  operatedNumbers.push(Number(numberInput.value));
+}
 
 function add(num1, num2) {
   return num1 + num2;

ea13c10d5232a270bfee790ee6248432481c96c2
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-13 14:13:50 +0700

    Add logic for all math operation

--- app.js
+++ app.js
@@ -29,8 +29,12 @@ function displayNumber(target) {
 function showResult(id) {
   if (id === "add-btn") {
     numberInput.value = add(operatedNumbers[0], operatedNumbers[1]);
+  } else if (id === "substraction-btn") {
+    numberInput.value = substract(operatedNumbers[0], operatedNumbers[1]);
   } else if (id === "multiply-btn") {
     numberInput.value = multiply(operatedNumbers[0], operatedNumbers[1]);
+  } else if (id === "division-btn") {
+    numberInput.value = divide(operatedNumbers[0], operatedNumbers[1]);
   }
   operatedNumbers.length = 0;
   operatedNumbers.push(Number(numberInput.value));
@@ -48,6 +52,6 @@ function multiply(num1, num2) {
   return num1 * num2;
 }
 
-function division(num1, num2) {
+function divide(num1, num2) {
   return num1 / num2;
 }
